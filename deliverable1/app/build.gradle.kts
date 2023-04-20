be4ad425fe3c447b7f0097135b03a12648fcd480
Navya <navyachowdary.nelluri@slu.edu>
2023-04-19 20:27:12 -0500

    version 3

--- deliverable1/app/build.gradle.kts
+++ deliverable1/app/build.gradle.kts
@@ -26,5 +26,5 @@ dependencies {
 
 application {
     // Define the main class for the application.
-    mainClass.set("FindMatch.Driver")
+    mainClass.set("FindMatch.App")
 }
--- deliverable1/app/src/main/java/FindMatch/App.java
+++ deliverable1/app/src/main/java/FindMatch/App.java
@@ -4,7 +4,7 @@ package FindMatch;
 import FindMatch.view.GameView;
 import FindMatch.controller.GameController;
 
-public class Driver {
+public class App {
     public static void main(String[] args) {
         GameController controller = new GameController();
     }
