40b3d897d9974039bb653dd9d31f4f40ade251a5
Navya <navyachowdary.nelluri@slu.edu>
2023-04-25 20:53:26 -0500

    appending score to file v1

--- app/src/main/java/FindMatch/model/MatchValidator.java
+++ app/src/main/java/FindMatch/model/MatchValidator.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
+import java.io.*;
 
 public class MatchValidator extends JFrame implements ActionListener {
    private JButton[] buttons;
@@ -21,93 +22,92 @@ public class MatchValidator extends JFrame implements ActionListener {
    private JLabel score;
    private int gridSize;
    private double gridDimension;
-   private int score_val=0;
-   public MatchValidator()
-   {
-   }
+   private int score_val = 0;
+   private String scoreFilePath = "scores.txt";
+
+   public MatchValidator() {}
 
    public MatchValidator(int gridSize) {
-    this.gridSize=gridSize;
-    this.gridDimension=Math.sqrt(gridSize);
+      this.gridSize = gridSize;
+      this.gridDimension = Math.sqrt(gridSize);
 
-    mainFrame = new JFrame("Memory Matcher");
-    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    mainFrame.setSize(400, 400);
-    mainFrame.setLayout(new BorderLayout());
+      mainFrame = new JFrame("Memory Matcher");
+      mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      mainFrame.setSize(400, 400);
+      mainFrame.setLayout(new BorderLayout());
 
-    mainPanel = new JPanel();
-    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+      mainPanel = new JPanel();
+      mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
-    buttonPanel = new JPanel(new GridLayout((int)gridDimension, (int)gridDimension));
-    buttonPanel.setPreferredSize(new Dimension(300, 300));
-    buttonPanel.setMaximumSize(new Dimension(300, 300));
+      buttonPanel = new JPanel(new GridLayout((int)gridDimension, (int)gridDimension));
+      buttonPanel.setPreferredSize(new Dimension(300, 300));
+      buttonPanel.setMaximumSize(new Dimension(300, 300));
 
-    buttons = new JButton[gridSize];
-    values = new int[gridSize];
-    found = new int[gridSize];
-	
-	startButton = new JButton("Start Game");
-    startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-    startButton.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
+      buttons = new JButton[gridSize];
+      values = new int[gridSize];
+      found = new int[gridSize];
+
+      startButton = new JButton("Start Game");
+      startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+      startButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
             showButtonsFor2Seconds();
-            }
-        });
+         }
+      });
 
-    // Generate random numbers for values array
-    ArrayList<Integer> list = new ArrayList<>();
-    for (int i = 0; i < gridSize/2; i++) {
-        list.add(i);
-        list.add(i);
-    }
-    Collections.shuffle(list);
-    for (int i = 0; i < gridSize; i++) {
-        buttons[i] = new JButton(" ");
-        buttons[i].addActionListener(this);
-        buttonPanel.add(buttons[i]);
-        values[i] = list.get(i);
-		found[i] = 0;
-    }
-    mainPanel.add(startButton);
-    mainPanel.add(Box.createRigidArea(new Dimension(0, 50)));
-    mainPanel.add(buttonPanel, BorderLayout.CENTER);
+      // Generate random numbers for values array
+      ArrayList<Integer> list = new ArrayList<>();
+      for (int i = 0; i < gridSize/2; i++) {
+         list.add(i);
+         list.add(i);
+      }
+      Collections.shuffle(list);
+      for (int i = 0; i < gridSize; i++) {
+         buttons[i] = new JButton(" ");
+         buttons[i].addActionListener(this);
+         buttonPanel.add(buttons[i]);
+         values[i] = list.get(i);
+         found[i] = 0;
+      }
+      mainPanel.add(startButton);
+      mainPanel.add(Box.createRigidArea(new Dimension(0, 50)));
+      mainPanel.add(buttonPanel, BorderLayout.CENTER);
 
-    status = new JLabel("STATUS: ALL THE BEST!");
-    status.setAlignmentX(Component.CENTER_ALIGNMENT);
-    mainPanel.add(Box.createRigidArea(new Dimension(0, 50)));
-    mainPanel.add(status);
+      status = new JLabel("STATUS: ALL THE BEST!");
+      status.setAlignmentX(Component.CENTER_ALIGNMENT);
+      mainPanel.add(Box.createRigidArea(new Dimension(0, 50)));
+      mainPanel.add(status);
 
-    score = new JLabel("SCORE: 0");
-    score.setAlignmentX(Component.CENTER_ALIGNMENT);
-    mainPanel.add(score);
-    mainFrame.add(mainPanel, BorderLayout.CENTER);
+      score = new JLabel("SCORE: 0");
+      score.setAlignmentX(Component.CENTER_ALIGNMENT);
+      mainPanel.add(score);
+      mainFrame.add(mainPanel, BorderLayout.CENTER);
 
-    mainFrame.pack();
-    mainFrame.setLocationRelativeTo(null);
-    mainFrame.setVisible(true);
-}
-    private void showButtonsFor2Seconds() {
-        // Disable the start button
-        startButton.setEnabled(false);
-		for (int i = 0; i < gridSize; i++) {
-        buttons[i].setText("" + values[i]);
-        buttons[i].setEnabled(false);
-    }
-    javax.swing.Timer timer = new javax.swing.Timer(2000, new ActionListener() {
-        public void actionPerformed(ActionEvent evt) {
+      mainFrame.pack();
+      mainFrame.setLocationRelativeTo(null);
+      mainFrame.setVisible(true);
+   }
+
+   private void showButtonsFor2Seconds() {
+      // Disable the start button
+      startButton.setEnabled(false);
+      for (int i = 0; i < gridSize; i++) {
+         buttons[i].setText("" + values[i]);
+         buttons[i].setEnabled(false);
+      }
+      javax.swing.Timer timer = new javax.swing.Timer(2000, new ActionListener() {
+         public void actionPerformed(ActionEvent evt) {
             for (int i = 0; i < gridSize; i++) {
-                if (found[i] == 0) {
-                    buttons[i].setText(" ");
-                    buttons[i].setEnabled(true);
-                }
+               if (found[i] == 0) {
+                  buttons[i].setText(" ");
+                  buttons[i].setEnabled(true);
+               }
             }
-        }
-    });
-    timer.setRepeats(false);
-    timer.start();
-    }
-
-
+         }
+      });
+      timer.setRepeats(false);
+      timer.start();
+   }
 
    public void actionPerformed(ActionEvent e) {
       int index = -1;
@@ -153,9 +153,17 @@ public class MatchValidator extends JFrame implements ActionListener {
 
       if (allFound()) {
          status.setText("STATUS: Congratulations! You found all matches.");
+         try {
+            FileWriter writer = new FileWriter("score.txt", true);
+            writer.write(score_val + "\n");
+            writer.close();
+            status.setText("STATUS: Score saved to file.");
+         } catch (IOException ex) {
+            ex.printStackTrace();
+         }
       }
+	  
    }
-
    public boolean allFound() {
       for (int i = 0; i < gridSize; i++) {
          if (found[i] == 0) {
