b55178aa39abc6656bd8fa11e0a8e82b9ad5c6f9
Josna Pitta <josna.pitta@slu.edu>
2023-05-03 11:35:16 -0500

    accepted changes

--- app/src/main/java/FindMatch/controller/GameController.java
+++ app/src/main/java/FindMatch/controller/GameController.java
@@ -10,28 +10,26 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
-public class GameController implements ControllerInterface {
+
+public class GameController implements ControllerInterface{
 	private int gridSize;
-	private GameView view;
-	private MatchValidator match;
-
-	public GameController() {
-		this.view = new GameView(this);
-	}
-
-<<<<<<< HEAD
-	public int getGridSize() {
-		return gridSize;
-	}
+    private GameView view;
+    private MatchValidator match;
 
     
-}
-=======
-	public void setGridSize(int gridSize) {
+    public GameController() {
+		this.view = new GameView(this);
+    }
+    
+	public void setGridSize(int gridSize)
+	{
 		this.gridSize = gridSize;
 		match = new MatchValidator(gridSize);
-		view.showGrid();
+        view.showGrid();
 	}
->>>>>>> a55c4a1cd76be3d1637c3a90c670b3d6d1a727c1
 
+	public int getGridSize() {
+		return gridSize;
+	}    
 }
+
--- app/src/main/java/FindMatch/view/GameView.java
+++ app/src/main/java/FindMatch/view/GameView.java
@@ -1,8 +1,6 @@
 package FindMatch.view;
 
 import FindMatch.controller.GameController;
-<<<<<<< HEAD
-import FindMatch.view.GameImage;
 
 import java.awt.*;
 import java.awt.GridBagConstraints;
@@ -23,35 +21,16 @@ public class GameView extends JFrame implements ActionListener,ObserverInterface
     private JFrame mainFrame;
 	private GameController controller;
     private JPanel contentPane;
-=======
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class GameView extends JFrame implements ObserverInterface, ActionListener {
-    private final JButton EasyBtn;
-    private final JButton MediumBtn;
-    private final JButton HardBtn;
-    private final GameController controller;
->>>>>>> a55c4a1cd76be3d1637c3a90c670b3d6d1a727c1
 
     public GameView(GameController controller) {
         super("Find a Match");
         this.controller = controller;
 
-<<<<<<< HEAD
-        //ImageIcon backgroundImage = new ImageIcon(getClass().getResource("/gameImage.jpg"));
-        //System.out.println(getClass().getResource("/gameImage.jpg"));
-        //mainFrame.setContentPane(new JLabel(backgroundImage));
+        mainFrame = new JFrame("Find a Match");
+        mainFrame.setPreferredSize(new Dimension(500,500));
 
         mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
-=======
-        // Set up main panel
-        JPanel mainPanel = new JPanel(new GridBagLayout());
->>>>>>> a55c4a1cd76be3d1637c3a90c670b3d6d1a727c1
         GridBagConstraints c = new GridBagConstraints();
         c.insets = new Insets(10, 10, 10, 10);
         c.gridwidth = GridBagConstraints.REMAINDER;
@@ -71,7 +50,6 @@ public class GameView extends JFrame implements ObserverInterface, ActionListene
         levelLbl.setFont(font1);
         mainPanel.add(levelLbl, c);
 
-<<<<<<< HEAD
         easyButton = new JButton("Easy");
         easyButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		easyButton.addActionListener(this);
@@ -94,31 +72,10 @@ public class GameView extends JFrame implements ObserverInterface, ActionListene
         mainFrame.pack();
         mainFrame.setPreferredSize(mainFrame.getSize());
         mainFrame.setVisible(true);
-=======
-        // Set up easy button
-        EasyBtn = createRectangularButton("Easy", font1, this);
-        mainPanel.add(EasyBtn, c);
-
-        // Set up medium button
-        MediumBtn = createRectangularButton("Medium", font1, this);
-        mainPanel.add(MediumBtn, c);
-
-        // Set up hard button
-        HardBtn = createRectangularButton("Hard", font1, this);
-        mainPanel.add(HardBtn, c);
-
-        // Set up main frame
-        setContentPane(mainPanel);
-        setSize(500, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
->>>>>>> a55c4a1cd76be3d1637c3a90c670b3d6d1a727c1
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-<<<<<<< HEAD
 	
          JButton source = (JButton) e.getSource();
          if (source.getText().equals("Easy")) {
@@ -131,18 +88,6 @@ public class GameView extends JFrame implements ObserverInterface, ActionListene
 	    mainFrame.setVisible(false);	
 	}    
     
-=======
-        JButton source = (JButton) e.getSource();
-        switch (source.getText()) {
-            case "Easy" -> controller.setGridSize(4);
-            case "Medium" -> controller.setGridSize(16);
-            case "Hard" -> controller.setGridSize(36);
-        }
-        setVisible(false);
-    }
-
-    @Override
->>>>>>> a55c4a1cd76be3d1637c3a90c670b3d6d1a727c1
     public void update() {
         showGrid();
     }
@@ -150,32 +95,9 @@ public class GameView extends JFrame implements ObserverInterface, ActionListene
     public void showGrid() {
         setVisible(true);
     }
-<<<<<<< HEAD
 	public void addLevelButtonListener(ActionListener listener) {
     easyButton.addActionListener(listener);
     hardButton.addActionListener(listener);
     mediumButton.addActionListener(listener);
 }
-=======
-
-    public void addLevelButtonListener(ActionListener listener) {
-        EasyBtn.addActionListener(listener);
-        HardBtn.addActionListener(listener);
-        MediumBtn.addActionListener(listener);
-    }
-
-    private JButton createRectangularButton(String text, Font font, ActionListener listener) {
-        JButton button = new JButton(text);
-        button.setAlignmentX(Component.CENTER_ALIGNMENT);
-        button.addActionListener(listener);
-        button.setBorder(BorderFactory.createCompoundBorder(
-                BorderFactory.createLineBorder(Color.BLACK, 2),
-                BorderFactory.createEmptyBorder(5, 15, 5, 15)));
-        button.setFocusPainted(false);
-        button.setBackground(Color.WHITE);
-        button.setFont(font);
-        return button;
-    }
-
->>>>>>> a55c4a1cd76be3d1637c3a90c670b3d6d1a727c1
 }
