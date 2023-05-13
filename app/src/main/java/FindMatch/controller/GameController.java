473321784e2b979c001103a63aff46da70cd9cfb
sairudhreddyvelapati <sairudhreddy.velapati@slu.edu>
2023-05-12 19:07:02 -0600

    init

--- app/src/main/java/FindMatch/controller/GameController.java
+++ app/src/main/java/FindMatch/controller/GameController.java
@@ -10,26 +10,22 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
-
-public class GameController implements ControllerInterface{
+public class GameController implements ControllerInterface {
 	private int gridSize;
-    private GameView view;
-    private MatchValidator match;
+	private GameView view;
+	private MatchValidator match;
 
-    
-    public GameController() {
+	public GameController() {
 		this.view = new GameView(this);
-    }
-    
-	public void setGridSize(int gridSize)
-	{
+	}
+
+	public void setGridSize(int gridSize) {
 		this.gridSize = gridSize;
 		match = new MatchValidator(gridSize);
-        view.showGrid();
+		view.showGrid();
 	}
 
 	public int getGridSize() {
 		return gridSize;
-	}    
+	}
 }
-
